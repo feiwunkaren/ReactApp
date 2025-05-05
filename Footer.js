7bdc68d56844f2e0d6bebc909330b4fe543c5e1e
Karen <feiwunkaren@gmail.com>
2025-05-05 01:35:27 -0400

    Added footer and header sections

---
+++ Footer.js
@@ -0,0 +1,14 @@
+<script>
+  // Example placeholder - could be used to handle form submissions
+  document.querySelector('.subscribe-btn').addEventListener('click', () => {
+    alert("Subscribed!");
+  });
+
+  document.querySelector('.submit-btn').addEventListener('click', () => {
+    alert("Suggestion submitted!");
+  });
+
+  document.querySelector('.contact-btn').addEventListener('click', () => {
+    alert("Redirecting to contact form...");
+  });
+</script>
